5ba1878640c698df640ba04d6ea724e6a07bd8c6
Manish0987654321 <manishghonge177@gmail.com>
2023-07-05 17:31:52 +0530

    /chnages/

--- resources/views/pages/Profile/profile.blade.php
+++ resources/views/pages/Profile/profile.blade.php
@@ -1,79 +1,4 @@
-<x-default-layout>
-
-
-    <!-- begin::Body -->
-    <div class="m-grid__item m-grid__item--fluid m-grid m-grid--ver-desktop m-grid--desktop m-body">
-
-        <!-- BEGIN: Left Aside -->
-        <button class="m-aside-left-close  m-aside-left-close--skin-dark " id="m_aside_left_close_btn"><i class="la la-close"></i></button>
-        <div id="m_aside_left" class="m-grid__item	m-aside-left  m-aside-left--skin-dark ">
-
-            <!-- BEGIN: Aside Menu -->
-            <div id="m_ver_menu" class="m-aside-menu  m-aside-menu--skin-dark m-aside-menu--submenu-skin-dark " m-menu-vertical="1" m-menu-scrollable="1" m-menu-dropdown-timeout="500">
-                <ul class="m-menu__nav ">
-                    <li class="m-menu__item " aria-haspopup="true"><a href="https://ortho.techasiamechatronics.com/dashboard" class="m-menu__link "><span class="m-menu__item-here"></span><i class="m-menu__link-icon fa fa-chart-bar"></i><span class="m-menu__link-text">Dashboard</span></a></li>
-                    
-
-                    <li class="m-menu__section ">
-                        <h4 class="m-menu__section-text">Manage</h4>
-                    </li>
-                    
-                    
-                        <li class="m-menu__item  m-menu__item--submenu " aria-haspopup="true" m-menu-submenu-toggle="hover"><a href="javascript:;" class="m-menu__link m-menu__toggle"><span class="m-menu__item-here"></span><i class="m-menu__link-icon m-menu__link-icon fa fa-file-medical-alt"></i><span class="m-menu__link-text">Test Data</span><i class="m-menu__ver-arrow la la-angle-right"></i></a>
-                            <div class="m-menu__submenu "><span class="m-menu__arrow"></span>
-                                <ul class="m-menu__subnav">
-                                    <li class="m-menu__item  m-menu__item--parent" aria-haspopup="true"><span class="m-menu__link"><span class="m-menu__item-here"></span><span class="m-menu__link-text">Test Data</span></span></li>
-                                    <li class="m-menu__item  " aria-haspopup="true" m-menu-link-redirect="1"><a href="https://ortho.techasiamechatronics.com/testdata" class="m-menu__link "><i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span class="m-menu__link-text">Test Data List</span></a></li>
-                                                                                <!-- <li class="m-menu__item  " aria-haspopup="true" m-menu-link-redirect="1"><a href="/testdata/add-new" class="m-menu__link "><i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span class="m-menu__link-text">Add new Test Data</span></a></li> -->
-                                                                            </ul>
-                            </div>
-                        </li>
-
-                                                
-                    <li class="m-menu__item  m-menu__item--submenu " aria-haspopup="true" m-menu-submenu-toggle="hover"><a href="javascript:;" class="m-menu__link m-menu__toggle"><span class="m-menu__item-here"></span><i class="m-menu__link-icon m-menu__link-icon fa fa-user-plus"></i><span class="m-menu__link-text">Patients</span><i class="m-menu__ver-arrow la la-angle-right"></i></a>
-                        <div class="m-menu__submenu "><span class="m-menu__arrow"></span>
-                            <ul class="m-menu__subnav">
-                                <li class="m-menu__item  m-menu__item--parent" aria-haspopup="true"><span class="m-menu__link"><span class="m-menu__item-here"></span><span class="m-menu__link-text">Patients</span></span></li>
-                                <li class="m-menu__item  " aria-haspopup="true" m-menu-link-redirect="1"><a href="https://ortho.techasiamechatronics.com/patients" class="m-menu__link "><i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span class="m-menu__link-text">Patients List</span></a></li>
-                                                                        <li class="m-menu__item  " aria-haspopup="true" m-menu-link-redirect="1"><a href="https://ortho.techasiamechatronics.com/patients/add-new" class="m-menu__link "><i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span class="m-menu__link-text">Add new Patient</span></a></li>
-                                                                    </ul>
-                        </div>
-                    </li>
-
-                    
-                        
-                                                    
-                    
-                                                        <li class="m-menu__section ">
-                                <h4 class="m-menu__section-text">Admin</h4>
-                            </li>
-
-                            <li class="m-menu__item  m-menu__item--submenu m-menu__item--active m-menu__item--expanded m-menu__item--open" aria-haspopup="true" m-menu-submenu-toggle="hover"><a href="javascript:;" class="m-menu__link m-menu__toggle"><span class="m-menu__item-here"></span><i class="m-menu__link-icon m-menu__link-icon fa fa-user-md"></i><span class="m-menu__link-text">Users</span><i class="m-menu__ver-arrow la la-angle-right"></i></a>
-                            <div class="m-menu__submenu "><span class="m-menu__arrow"></span>
-                                <ul class="m-menu__subnav">
-                                    <li class="m-menu__item  m-menu__item--parent" aria-haspopup="true"><span class="m-menu__link"><span class="m-menu__item-here"></span><span class="m-menu__link-text">Users</span></span></li>
-                                    <li class="m-menu__item  " aria-haspopup="true" m-menu-link-redirect="1"><a href="https://ortho.techasiamechatronics.com/users" class="m-menu__link "><i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span class="m-menu__link-text">Users List</span></a></li>
-                                                                                <li class="m-menu__item  m-menu__item--active" aria-haspopup="true" m-menu-link-redirect="1"><a href="https://ortho.techasiamechatronics.com/users/add-new" class="m-menu__link "><i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span class="m-menu__link-text">Add new User</span></a></li>
-                                                                            </ul>
-                            </div>
-                        </li>
-                            
-                            <li class="m-menu__item " aria-haspopup="true" m-menu-link-redirect="1"><a href="https://ortho.techasiamechatronics.com/logger" class="m-menu__link"><span class="m-menu__item-here"></span><i class="m-menu__link-icon fa fa-fingerprint"></i><span class="m-menu__link-text">Activity Log</span></a></li>
-                                                    
-                        <li class="m-menu__section ">
-                            <h4 class="m-menu__section-text">Settings</h4>
-                        </li>
-                        <li class="m-menu__item " aria-haspopup="true" m-menu-link-redirect="1"><a href="https://ortho.techasiamechatronics.com/profile" class="m-menu__link"><span class="m-menu__item-here"></span><i class="m-menu__link-icon fa fa-user-circle"></i><span class="m-menu__link-text">Profile</span></a></li>
-                </ul>
-            </div>
-
-            <!-- END: Aside Menu -->
-        </div>
-
-        <!-- END: Left Aside -->
-        <div class="m-grid__item m-grid__item--fluid m-wrapper">
-
-
+<x-default-layout></x-default-layout>
 <div class="m-subheader ">
 <div class="d-flex align-items-center">
 <div class="mr-auto">
@@ -96,7 +21,8 @@
                         <i class="la la-gear"></i>
                     </span>
                     <h3 class="m-portlet__head-text">
-                        Add New user							</h3>
+                        Add New user
+                    </h3>
                 </div>
             </div>
         </div>
